5208aaca8b355967e1dccb9306d5a0b295d639cd
Katie Kruzan <31875578+katiekruzan@users.noreply.github.com>
2021-10-03 16:00:35 -0500

    checking in on the documentation

--- local-rotations.py
+++ local-rotations.py
@@ -13,13 +13,14 @@ import sys  # This just helps us in our printing
 # Getting the structure for the classes we're putting together
 class Segment:
     """
-    These are going to represent the outer segments and the mysteries they hold
+    These are going to represent the outer segments and the mysteries they hold.
+    The segments will be adjacent to 2 outer nodes
     """
 
     def __init__(self, name: str):
         """
         Initialize the segment, keeping a place for the right left outer vertices to which it is adjacent
-        :param name: How we will reference this segment. In this implementation, it is expected to be a negative number
+        :param name: How we will reference this segment. In this implementation, it is expected to be a negative integer
         """
         self.leftOuter = None
         self.rightOuter = None
@@ -50,7 +51,7 @@ class Segment:
         """
         Set the outer node to the left of this segment with respect to the origin
         Also, set left's right segment to this segment.
-        :param left: A outer node object to be referenced as this node's left outer node
+        :param left: A outer node object to be referenced as this segment's left outer node
         :return: None
         """
         self.leftOuter = left
@@ -61,7 +62,7 @@ class Segment:
         """
         Set the outer node to the right of this segment with respect to the origin
         Also, set right's left segment to this segment.
-        :param right: A outer node object to be referenced as this node's right outer node
+        :param right: A outer node object to be referenced as this segment's right outer node
         :return: None
         """
         self.rightOuter = right
@@ -79,8 +80,8 @@ class Segment:
 
     def toString(self) -> str:
         """
-        
-        :return:
+        Returns a formatted string of the left and right outer nodes this is associated with
+        :return: Description string
         """
         return 'left Outer: ' + self.leftOuter.getName() + '\nright Outer: ' + self.rightOuter.getName()
 
@@ -90,46 +91,93 @@ class Outer:
     Class to represent the outer vertices that are adjacent to an inner vertex and 2 outer segments
     """
 
-    def __init__(self, name):
+    def __init__(self, name: str):
+        """
+        Initialize the outer node
+
+        Keeping a place for the inner vertex and right and left outer segments to which it is adjacent.
+        :param name: How we will reference this outer node. In this implementation, it is expected to be a positive integer
+        """
         self.adjInner = None
         self.leftSegment = None
         self.rightSegment = None
         self.name = name
 
-    def getName(self):
+    def getName(self) -> str:
+        """
+        Return the name we gave to this outer node.
+        :return: name
+        """
         return self.name
 
-    def getLeftSegment(self):
+    def getLeftSegment(self) -> Segment:
+        """
+        Return the segment object to the left of this outer node with respect to the origin
+        :return: leftSegment
+        """
         return self.leftSegment
 
-    def getRightSegment(self):
+    def getRightSegment(self) -> Segment:
+        """
+        Return the segment object to the right of this outer node with respect to the origin
+        :return: rightSegment
+        """
         return self.rightSegment
 
     def getAdjInner(self):
+        """
+        Return the inner node object adjacent to this outer note object
+        :return: adjInner
+        """
         return self.adjInner
 
-    def setLeftSegment(self, left):
-        # this is a segment
+    def setLeftSegment(self, left: Segment):
+        """
+        Set the segment to the left of this outer node with respect to the origin
+        Also, set left's right outer node to self.
+        :param left: A segment object to be referenced as this node's left outer segment
+        :return: None
+        """
         self.leftSegment = left
         if left.getRightOuter() is None:
             left.setRightOuter(self)
 
-    def setRightSegment(self, right):
+    def setRightSegment(self, right: Segment):
+        """
+        Set the segment to the right of this outer node with respect to the origin
+        Also, set right's left outer node to self.
+        :param right: A segment object to be referenced as this node's right outer segment
+        :return: None
+        """
         self.rightSegment = right
         if right.getLeftOuter() is None:
             right.setLeftOuter(self)
 
     def setAdjInner(self, inner):
+        """
+        Set the inner node adjacent to this outer node
+        Also, set inner's adjacent outer node to self.
+        :param inner: A inner node object to be referenced as this node's adjacent inner node
+        :return: None
+        """
         self.adjInner = inner
         if inner.getAdjOuter() is None:
             inner.setAdjOuter(self)
 
-    def isValidObject(self):
+    def isValidObject(self) -> bool:
+        """
+        Checks to see if this outer node has been full initialized.
+        :return: valid returns true if it has the left segment, right segment, and inner node set
+        """
         if (self.leftSegment is None) or (self.rightSegment is None) or (self.adjInner is None):
             return False
         return True
 
-    def toString(self):
+    def toString(self) -> str:
+        """
+        Returns a formatted string of the left segment, right segment, and inner node this outer node is associated with
+        :return: Description string
+        """
         return 'left Segment: ' + self.leftSegment.getName() + '\nright Segment: ' + self.rightSegment.getName() \
                + '\nadj Inner: ' + self.adjInner.getName()
 
@@ -139,82 +187,137 @@ class Inner:
     Class to represent the inner vertices that are adjacent to an outer vertex and 2 neighboring inner vertices
     """
 
-    def __init__(self, name):
+    def __init__(self, name: str):
+        """
+        Initialize the inner node object
+
+        Keeping a place for the outer vertex and right and left adjacent inner nodes.
+        :param name: How we will reference this inner node. In this implementation, it is expected to be a lowercase letter
+        """
         self.adjOuter = None
         self.leftInner = None
         self.rightInner = None
         self.name = name
 
-    def getName(self):
+    def getName(self) -> str:
+        """
+        Return the name we gave to this inner node.
+        :return: name
+        """
         return self.name
 
     def getLeftInner(self):
+        """
+        Return the inner node object to the left of this inner node with respect to the origin
+        :return: leftInner
+        """
         return self.leftInner
 
     def getRightInner(self):
+        """
+        Return the inner node object to the right of this inner node with respect to the origin
+        :return: rightInner
+        """
         return self.rightInner
 
-    def getAdjOuter(self):
+    def getAdjOuter(self) -> Outer:
+        """
+        Return the outer node object adjacent to this inner node
+        :return: adjOuter
+        """
         return self.adjOuter
 
     def setLeftInner(self, left):
+        """
+        Set the inner node to the left of this inner node with respect to the origin
+        Also, set left's right inner node to self.
+        :param left: An inner node object to be referenced as this node's left inner node
+        :return: None
+        """
         self.leftInner = left
         if left.getRightInner() is None:
             left.setRightInner(self)
 
     def setRightInner(self, right):
+        """
+        Set the inner node to the right of this inner node with respect to the origin
+        Also, set right's left inner node to self.
+        :param right: An inner node object to be referenced as this node's right inner node
+        :return: None
+        """
         self.rightInner = right
         if right.getLeftInner() is None:
             right.setLeftInner(self)
 
-    def setAdjOuter(self, outer):
+    def setAdjOuter(self, outer: Outer):
+        """
+        Set the outer node adjacent to this inner node
+        Also, set outer's adjacent inner node to self.
+        :param outer: An outer node object to be referenced as this node's adjacent outer node
+        :return: None
+        """
         self.adjOuter = outer
         if outer.getAdjInner() is None:
             outer.setAdjInner(self)
 
-    def isValidObject(self):
+    def isValidObject(self) -> bool:
+        """
+        Checks to see if this inner node has been full initialized.
+        :return: valid returns true if it has the left inner node, right inner node, and adjacent outer node set
+        """
         if (self.leftInner is None) or (self.rightInner is None) or (self.adjOuter is None):
             return False
         return True
 
-    def toString(self):
+    def toString(self) -> str:
+        """
+        Returns a formatted string of the left inner node, right inner node, and adjacent outer node this inner node
+        is associated with
+        :return: Description string
+        """
         return 'left Inner: ' + self.leftInner.getName() + '\nright Inner: ' + self.rightInner.getName() \
                + '\nadj Outer: ' + self.adjOuter.getName()
 
 
-def standardCircle(num_verts):
+def standardCircle(num_verts: int):
     """
-    This will go through and initialize our standard circle
-    :param num_verts:
-    :return:
+    This will go through and initialize our standard starting circle
+    :param num_verts: the number of outer nodes we will have
+    :returns: tuple(segs, outs, inns)
+        -segs - dictionary of str: Segment objects in the circle \\
+        -outs - dictionary of str: Outer objects in the circle \\
+        -inns - dictionary of str: Inner objects in the circle
     """
+    # Initializing our dictionaries
     segs = dict()
     outs = dict()
     inns = dict()
 
+    # Running through the number of vertices we will be edning up with
     for i in range(num_verts):
-        # For labeling, we will use the standard labeling for inner and outer, and then negative numbers, etc for
-        # the segments
-
-        # start inner
+        # start with an inner node - labeling with lowercase letters
         inn = Inner(string.ascii_letters[i])
+        # If we aren't on the first one, connect it to the previous one.
         if i != 0:
             inn.setLeftInner(inns[string.ascii_letters[i - 1]])
-            if i == num_verts - 1:  # time to close up the circle
+            # If we've hit the end of the line, go ahead and close up the circle.
+            if i == num_verts - 1:
                 inn.setRightInner(inns[string.ascii_letters[0]])
 
         # then make the outer
         out = Outer(str(i + 1))
+        # Go ahead and connect the inner we just made with this outer node
         out.setAdjInner(inn)
+        # If we aren't on the first one, go ahead and connect it to the previous segment
         if i != 0:
             out.setLeftSegment(segs[str(-i)])
-            # if i == num_verts - 1:  # time to close up the circle
-            #     out.setLeftSegment(segs[str(-1)])
 
-        # then make the segment
+        # Now time to make the segment
         seg = Segment(str(-i - 1))
+        # Go ahead and connect the outer node we just made with this segment
         seg.setLeftOuter(out)
-        if i == num_verts - 1:  # time to close up the circle
+        # If we're at the end of the circle, then we close it up. Otherwise, move on
+        if i == num_verts - 1:
             seg.setRightOuter(outs[str(1)])
 
         # add them to our dictionaries
@@ -222,15 +325,25 @@ def standardCircle(num_verts):
         outs[out.getName()] = out
         inns[inn.getName()] = inn
 
+    # If we've made it here, then we've made the full circle and are ready to return it
     return segs, outs, inns
 
 
-def findTheFace(source_inner):
-    # order will be i, o, s, o, i repeat
-    face = []
-    source_in = source_inner
+def findTheFace(source_in: Inner) -> list:
+    """
+    This will take an inner node and use the algorithm to walk the face that it is on.
+    The order of the face will be i, o, s, o, i repeat
+    :param source_in: Inner node object we are starting from.
+    :return: face: a list representing the face. This list is of inner, outer, and segment objects in the
+            order i, o, s, o, i, repeat.
+    """
+    # initialize the list
+    face = list()
+    # starting the face with the source inner node.
     face.append(source_in)
+    # initialize the ending inner node we will be using for comparison
     end_in = None
+    # As long as we haven't looped back around, go through the following process.
     while source_in != end_in:
         # inner: find adjacent outer
         face.append(face[-1].getAdjOuter())
@@ -241,29 +354,43 @@ def findTheFace(source_inner):
         # outer: then adj inner
         face.append(face[-1].getAdjInner())
         # then left inner and repeat.
+        # set this inner node as our node to compare to our starting node.
         end_in = face[-1].getLeftInner()
         face.append(end_in)
     return face
 
 
-def faceCannonOrder(face):
+def faceCannonOrder(face: list) -> list:
     """
-    just a list with the face elements in order
-    :param face:
+    Just list the face with the face elements in order.
+    We will do it with the first numerical face, and then go right before it for an order that will be consistent.
+    :param face: a list representing the face. This list is of inner, outer, and segment objects in the
+            order i, o, s, o, i, repeat.
     :return: ordered face in canonical order
     """
     # find the first numerical face then go right before it
+    # initialize face num as a relatively high number we won't encounter
     facenum = 333
+    # initialize the int for where we will split the list
     start_ind = 0
+    # loop through and find the face we want to find
     for i in range(len(face)):
         try:
             if int(face[i].getName()) < facenum:
+                # To get here, we must have found a lower face
+                # keep track of where this is located in the list
                 start_ind = i - 1
+                # make our current lowest face the new lowest face to keep comparing to.
                 facenum = int(face[i].getName())
+        # if we try casting a letter to a number, python will get upset, but that also means we're looking at
+        # an inner node, which we don't want for this anyways.
         except ValueError:
             continue
-    # get rid of doubles
+
+    # make our ordered face getting from the starting index to the end, then wrapping around and getting the rest of
+    # the face
     ord_face = face[start_ind:] + face[:start_ind]
+    # go through and make sure we don't have any duplicate elements right by each other. If we do, then drop them.
     for i in range(len(ord_face) - 1):
         if ord_face[i].toString() == ord_face[i + 1].toString():
             ord_face.pop(i)
